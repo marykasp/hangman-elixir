05b26d309c291a765e9eb82860f80c5ae8a61735
marykasp <kasparian.mary@gmail.com>
2023-06-25 12:11:36 -0500

    Start Hangman Liveview

---
+++ b2/lib/b2_web/controllers/page_html.ex
@@ -0,0 +1,5 @@
+defmodule B2Web.PageHTML do
+  use B2Web, :html
+
+  embed_templates "page_html/*"
+end
---
+++ b2/lib/b2_web/router.ex
@@ -0,0 +1,37 @@
+defmodule B2Web.Router do
+  use B2Web, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_live_flash
+    plug :put_root_layout, html: {B2Web.Layouts, :root}
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", B2Web do
+    pipe_through :browser
+
+    get "/", PageController, :home
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", B2Web do
+  #   pipe_through :api
+  # end
+
+  # Enable Swoosh mailbox preview in development
+  if Application.compile_env(:b2, :dev_routes) do
+
+    scope "/dev" do
+      pipe_through :browser
+
+      forward "/mailbox", Plug.Swoosh.MailboxPreview
+    end
+  end
+end
