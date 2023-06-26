a5435a85bb6853d5d4ecd53433fbc0bd78a38509
marykasp <kasparian.mary@gmail.com>
2023-06-26 13:57:27 -0500

    Start work on word so far component.
    Add in game state, use a helper function to display a sentence instead
    of the state itself. Also display each letter from tally.letters (either
    a letter will be displayed if guess is good or will be an underscore).
    
    Add a `phx-window-keyup` handle event to the game liveview div. Will
    invoke `make_move` event if any key is pressed. Will pass that key as
    the guess to `Hangman.make_move` and then the socket will be updated
    with the new tally after each event.

--- b2/lib/b2_web/live/game.ex
+++ b2/lib/b2_web/live/game.ex
@@ -2,21 +2,36 @@ defmodule B2Web.Live.Game do
   use B2Web, :live_view
 
   def mount(_params, _session, socket) do
+    # create new hangman server process - starts individual game
     game = Hangman.new_game()
+    # use process of game to get tally
     tally = Hangman.tally(game)
 
+    # assign both to socket
     {:ok,
      socket
-     |> assign(%{game: game, tally: tally})}
+     |> assign(%{page_title: "Hangman", game: game, tally: tally})}
   end
 
+  @doc """
+  Liveview components are a good way of splitting a page into separate areas, each with its own substate and logic.
+  """
   def render(assigns) do
     ~H"""
-    <div class="game-holder">
+    <div class="game-holder" phx-window-keyup="make_move">
       <.live_component module={__MODULE__.Figure} id={1} tally={@tally} />
       <.live_component module={__MODULE__.Alphabet} id={2} tally={@tally} />
       <.live_component module={__MODULE__.WordSoFar} id={3} tally={@tally} />
     </div>
     """
+  end
+
+  def handle_event("make_move", %{"key" => key}, socket) do
+    # make move based on key
+    tally = Hangman.make_move(socket.assigns.game, key)
+
+    {:noreply,
+     socket
+     |> assign(:tally, tally)}
   end
 end
--- b2/lib/b2_web/live/game/word_so_far.ex
+++ b2/lib/b2_web/live/game/word_so_far.ex
@@ -1,13 +1,39 @@
 defmodule B2Web.Live.Game.WordSoFar do
   use B2Web, :live_component
 
+  @states %{
+    initializing: "Type or click on your first letter guess.",
+    already_used: "You already picked that letter!",
+    bad_guess: "That letter is not in the word.",
+    good_guess: "Good guess!",
+    lost: "Sorry, you lost...",
+    won: "You won!"
+  }
+
   def mount(socket) do
     {:ok, socket}
   end
 
   def render(assigns) do
     ~H"""
-    <h1>word_so_far</h1>
+    <div>
+      <div class="game-state">
+        <%= state_name(@tally.game_state) %>
+      </div>
+
+      <div class="letters flex items-center space-x-5 text-2xl w-[1em] text-violet-500">
+        <%= for letter <- @tally.letters do %>
+          <div class={"one-letter #{if letter != "_", do: "correct text-emerald-500"}"}>
+            <%= letter %>
+          </div>
+        <% end %>
+      </div>
+    </div>
     """
+  end
+
+  def state_name(state) do
+    # map state to a string
+    @states[state] || "Unknown state"
   end
 end
